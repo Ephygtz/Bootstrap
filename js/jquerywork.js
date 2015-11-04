ac621fc964323d2bad048a27a0103055cdd1fe81
Ephraim <ephygtz@gmail.com>
2015-11-04 17:12:57 +0300

    Add jQuery form validator

--- js/jquerywork.js
+++ js/jquerywork.js
@@ -11,4 +11,40 @@ $(document).ready(function() {
 	$('#hover').mouseleave(function(){
 		$('#unhide h5').hide(1500);
 	})
+	$('#contact-form').on('submit', function(e) {
+		url = "/";
+		data = $(this).serialize();
+		console.log(data);
+
+		$.post(url, data)
+		.done(function() {
+			alert("You're logged inn");
+		}).fail(function() {
+			alert("ooops, something went wrong")
+		});
+
+		e.preventDefault();
+	});
+
+	//Form user inputs validation
+	$('#contact-form').validate(
+	{
+		rules: {
+			fullname: "required",
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		messages: {
+			fullname: "Please enter your full names",
+			email: {
+				required: "Email cannot be empty!!",
+				email: "Please use this format someone@domain.com"
+			}
+		}
+
+	});
+
+
 });
